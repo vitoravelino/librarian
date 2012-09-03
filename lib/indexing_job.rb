4f6e93f76702bde839616d826a9103fbafe84be1
Vítor Avelino <contact@vitoravelino.net>
2012-09-03 12:17:21 -0300

    small refactoring on indexing job

--- lib/indexing_job.rb
+++ lib/indexing_job.rb
@@ -2,14 +2,29 @@ require 'net/http'
 
 class IndexingJob < Struct.new(:thesis)
   def perform
-    original_file = Rails.root.join('public', 'uploads', "#{thesis.id}#{thesis.file}").to_s
-    Docsplit.extract_text([original_file], :ocr => false, :output => 'public/uploads/')
-    tmp_file = original_file.sub('.pdf', '.txt')
-    thesis.file = "#{thesis.id}#{thesis.file}"
-    thesis.text = File.read(tmp_file)
-    thesis.save
-    thesis.solr_index!
-    File.delete(tmp_file) if File.exists?(tmp_file)
+    tmp_file = extract_content(thesis)
+    update_thesis(File.read(tmp_file))
+    delete_file(tmp_file)
   end
+
+  private
+    def extract_content(thesis)
+      original_file = Rails.root.join('public', 'uploads', "#{thesis.id}#{thesis.file}").to_s
+      Docsplit.extract_text([original_file], :ocr => false, :output => 'public/uploads/')
+      tmp_file = original_file.sub('.pdf', '.txt')
+
+      tmp_file
+    end
+
+    def update_thesis(text)
+      thesis.file = "#{thesis.id}#{thesis.file}"
+      thesis.text = text
+      thesis.save
+      thesis.solr_index!
+    end
+
+    def delete_file(filename)
+      File.delete(filename) if File.exists?(filename)
+    end
 
 end
