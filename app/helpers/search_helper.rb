11c252f151c7ec5f4793bb3e2142d867ee0169d9
Vítor Avelino <contact@vitoravelino.net>
2012-09-03 12:17:07 -0300

    small fixing around text and its highlight

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,13 @@
+require 'active_support/all'
+
 module SearchHelper
+  
+  def excerpt(hit)
+    if not hit.highlights(:text).empty? then
+      field = sanitize hit.highlights(:text).first.format { |word| "<strong>#{word}</strong>" }
+    end
+
+    concat(field || hit.instance.text[0..100])
+  end
+
 end
